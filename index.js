adc579049395aca1880b3497204346bacc9cdfba
Benjamin Habegger <benjamin@boot-start.com>
2018-03-26 14:35:13 +0200

    Initialized calulations

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const io = require('socket.io')(server);
 
 
 var taux = 20;
+var lastClient = 0;
+var clients = {};
 
 app.get('/', function(req, res) {
     res.send("Hello World!");
@@ -30,14 +32,39 @@ visu.on('connection', function(socket){
 });
 
 function updateVisu() {
+   console.log(clients);
+   var count = 0;
+   var sum = 0;
+   for(c in clients) {
+     if(clients[c]['start']) {
+       sum += Number(clients[c]['start']);
+       count++;
+     }
+   }
+   console.log('Sum:',sum);
+   taux = sum / count;
+   console.log('New taux:',taux);
    visu.emit('taux',taux);
 }
 
 var mobile = io.of('/mobile');
 mobile.on('connection', function(socket) {
+    var clientId = lastClient++;
+    clients[clientId] = {};
     socket.on('start', function(v) {
-        console.log('new start ',v);
-        taux = v;
+        console.log('client[',clientId,'].start=',v);
+        clients[clientId]['start'] = v;
+        updateVisu();
+    });
+
+    socket.on('end', function(v) {
+        console.log('client[',clientId,'].end=',v);
+        clients[clientId]['end'] = v;
+        updateVisu();
+    });
+
+    socket.on('disconnect', function() {
+        delete clients[clientId];
         updateVisu();
     });
 });
