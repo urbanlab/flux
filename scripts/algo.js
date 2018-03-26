62776cf50ea62c205fed7ab3bc4721cef0aa7448
afourcat <alexandre.fourcat@epitech.eu>
2018-03-27 00:08:38 +0200

    Adding hour conversion

--- scripts/algo.js
+++ scripts/algo.js
@@ -5,17 +5,24 @@ const path = require('path');
 
 module.exports = {
 
-//Sort all users
+//Sort all user
 sort_users: function () {
 	prob_array = get_prob_array('./prob_file.json');
 	
-	return (sorted_histogram);
+	//TODO: Adding algo
+	return (prob_array);
 }
 };
 
 function get_prob_array(path_to_file) {
-
 	file = fs.readFileSync(path_to_file, {encoding: 'utf-8'});
 	ret = JSON.parse(file);
 	return (ret);
-}
+};
+
+function hour_to_index(hour) {
+	trunc_hour = parseInt(hour);
+	minutes = hour - trunc_hour;
+	index = ((trunc_hour - 6) * 4) + Math.round(minutes / 0.15)
+	return (index);
+};
