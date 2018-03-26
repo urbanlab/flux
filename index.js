811e4290cbbce9e52522f26f269de07f355e09a1
afourcat <alexandre.fourcat@epitech.eu>
2018-03-26 23:40:45 +0200

    Adding prob array and Json parsing

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ mobile.on('connection', function(socket) {
 
     //On start message stock the value in client[id]
     socket.on('start', function(v) {
+	algo.sort_users();
         console.log('client[',clientId,'].start=',v);
         clients[clientId]['start'] = v;
         utils.updateVisu(clients, visu);
--- scripts/algo.js
+++ scripts/algo.js
@@ -1,7 +1,21 @@
 //Javascript file for sorting functions
 
+const fs   = require('fs');
+const path = require('path');
+
 module.exports = {
 
+//Sort all users
+sort_users: function () {
+	prob_array = get_prob_array('./prob_file.json');
+	
+	return (sorted_histogram);
+}
 };
 
+function get_prob_array(path_to_file) {
 
+	file = fs.readFileSync(path_to_file, {encoding: 'utf-8'});
+	ret = JSON.parse(file);
+	return (ret);
+}
