73aedafce3d86b42f6e1b000b5a1e85804c12678
Benjamin Habegger <benjamin@boot-start.com>
2018-03-26 14:01:18 +0200

    added basic communication

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
+
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+
+var taux = 20;
 
 app.get('/', function(req, res) {
     res.send("Hello World!");
@@ -13,6 +19,26 @@ app.get('/visu', function(req, res) {
    res.sendfile('views/visu/index.html');
 });
 
-app.listen(3000, function() {
+server.listen(3000, function() {
     console.log('FLUX Server started on port 3000');
+});
+
+var visu = io.of('/visu');
+visu.on('connection', function(socket){
+  console.log('someone connected on visu');
+  socket.emit('taux', taux);
+});
+
+var mobile = io.of('/mobile');
+mobile.on('connection', function(socket) {
+    socket.on('start', function(v) {
+        console.log('new start ',v);
+        visu.emit('taux', v);
+    });
+});
+
+app.get('/update', function(req, res) {
+ taux = 30;
+ visu.emit('taux', taux);
+ res.send('{}');
 });
