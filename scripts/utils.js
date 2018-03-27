876e5536d7fa89a8eeeb1ecc5c86c517acd15f04
Benjamin Habegger <benjamin@boot-start.com>
2018-03-27 17:18:38 +0200

    First integration

--- scripts/utils.js
+++ scripts/utils.js
@@ -3,16 +3,20 @@
 
 
 module.exports = {
-updateVisu: function (clients) {
+updateVisu: function (visu, clients) {
 		HT = prob_array;
+		/*
 		for (var i in clients) {
 			if (clients[i]['start'] && clients[i]['end']) {
 				HT = make_histo(HT, clients[i]['start'], clients[i]['end'], 300);
 			}
 			console.log('histogram = ', HT);
-			visu.emit('histogram', HT);
+
 		}
-	     },
+		*/
+		console.log("histogram", HT);
+		visu.emit('histogram', HT);
+    },
 
 color_changer: function (index, histogram) {
 		if (histogram[index] > 9) {
