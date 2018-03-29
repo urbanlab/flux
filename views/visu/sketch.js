9d1ff70ad07d44f3baf3eb5fbf5f1ce85d6179d8
bastien crozet <crozet.bastien1@gmail.com>
2018-03-29 19:17:37 +0200

    added traffic simulation

--- views/visu/sketch.js
+++ views/visu/sketch.js
@@ -3,8 +3,8 @@ var histogram;
 
 var time = ['06:00', '06:15', '06:30', '06:45', '07:00', '07:15', '07:30', '07:45', '08:00', '08:15', '08:30', '08:45', '09:00', '09:15', '09:30', '09:45', '10:00', '10:15', '10:30', '10:45' ,'11:00', '11:15', '11:30', '11:45', '12:00'];
 var flux = [
-	[600,1000], [600, 980], [600,960], [600, 940], [600,920], [600, 900], [600, 880], [600,860], [600, 840], [600,820], [600, 800], [600, 780], [600,760], [600, 740], [600,720], [600, 700], [600, 680], [600,660], [600, 640], [600,620], [600, 600], [600,580], [602,560], [605,540], [609,520], [614,500], [620,480], [627,460], [635,440]
-];
+	[600, 880], [600,860], [600, 840], [600,820], [600, 800], [600, 780], [600,760], [600, 740], [600,720], [600, 700], [600, 680], [600,660], [600, 640], [600,620], [600, 600], [600,580], [602,560], [605,540], [609,520], [614,500], [620,480], [627,460], [635,440], [645,420], [656,400], [668,380], [682,360], [700,340], [700,340], [717,320], [740,300], [763,280], [788,260], [820,240], [856,220], [890,205], [925,193], [955,183], [985,176], [1015,170], [1045,167], [1075,165], [1098,165], [1110,190], [1130,205], [1110,190], [1155,220], [1155,240], [1155,260], [1155,280], [1155,300], [1155,320], [1155,340], [1155,360], [1155,380], [1155,400], [1155,420], [1155,440], [1155,460], [1155,480], [1155,500], [1155,520], [1155,540], [1155,560], [1155,580], [1155,600], [1155,620], [1155,640], [1155,660], [1155,680], [1155,700], [1155,720], [1155,740], [1155,760], [1155,780], [1155,800], [1155,820], [1155,840], [1175,850], [1195,850], [1215,850], [1235,850], [1255,850], [1275,850], [1295,850], [1320,850], [1320, 830]
+]
 //var cars;
 //var button;
 
@@ -19,9 +19,12 @@ var colorRed = 0;
 var congestion = 1;
 
 var flashlights = new Array();
-var flashlightsDensity = 5;
+var flashlightsDensity = 1;
 var flashlightsFramesCountMax = 15;
 var flashlightsFramesCount = 0;
+var flashlightsSlowFramesCountMax = 5;
+var flashlightsSlowFramesCount = 0;
+var flashlightsSlowIndex = 43;
 
 function preload() {
   Montserrat = loadFont('/assets-visu/assets/Montserrat-Regular.ttf');
@@ -67,6 +70,9 @@ function setup() {
 function updateColor(_histogram, index) {
   histogram = _histogram;
   congestion = histogram[index];
+
+	flashlightsDensity = int(map(congestion, 1, 13, 2000, 500));
+
   console.log('congestion:',congestion);
 }
 
@@ -444,7 +450,7 @@ image(img, 0, 0, windowWidth, windowHeight);
 
     //Route sud
       //fond
-        stroke(230);
+        stroke(164);
         strokeWeight(30);
         strokeCap(ROUND);
         line(1155, 214, 1155, 2000);
@@ -457,7 +463,7 @@ image(img, 0, 0, windowWidth, windowHeight);
 
     //Droite vers entreprise
       //fond
-        stroke(230);
+        stroke(164);   // Sandro a modifié ça
         strokeWeight(15);
         strokeCap(SQUARE);
         line(1144, 850, 1375, 850);
@@ -498,12 +504,14 @@ image(img, 0, 0, windowWidth, windowHeight);
        if (histogram.hasOwnProperty(i)) {
         noStroke();
         fill(255);
+				var color = int(map(histogram[i], congestionMin, congestionMax, colorGreen, colorRed));
+				fill(min(color, colorGreen), 70, 100);
         if (i == slider.value()) {
           stroke(255);
           strokeWeight(5);
           fill(min(congestionColor+congestionPropagation, colorGreen), 100, 100);
         }
-        rect(lar * i, 1080-histogram[i]*15, lar+1, histogram[i]*15);
+        rect(lar * i, 1080-histogram[i]*12, lar+1, histogram[i]*12);
        }
     }
     for(var j in time) {
@@ -522,8 +530,9 @@ image(img, 0, 0, windowWidth, windowHeight);
     }
     colorMode(RGB, 255);
 		ellipseMode(CENTER);
-/*
+
 		flashlightsFramesCount++;
+		flashlightsSlowFramesCount++;
 
 		if (flashlightsFramesCount == flashlightsFramesCountMax) {
 			if (flashlights.length == 0) {
@@ -531,14 +540,11 @@ image(img, 0, 0, windowWidth, windowHeight);
 			}
 
 			for(var i = 0; i < flashlights.length; i++) {
-				flashlights[i]++;
-				if (flashlights[i] > flux.length) {
-					flashlights.splice(i, 1); // remove light
-				}
+				if (flashlights[i] < flashlightsSlowIndex) {
+					flashlights[i]++;
 
-				if (i == 0) {
-					if (flashlights[i] == flashlightsDensity) {
-						flashlights.unshift(-1); // add light
+					if (flashlights[i] > flux.length) {
+						flashlights.splice(i, 1); // remove light
 					}
 				}
 			}
@@ -546,22 +552,45 @@ image(img, 0, 0, windowWidth, windowHeight);
 			flashlightsFramesCount = 0;
 		}
 
+
+		if (flashlightsSlowFramesCount == flashlightsSlowFramesCountMax) {
+			if (flashlights.length == 0) {
+				flashlights.unshift(-1); // add light
+			}
+
+			for(var i = 0; i < flashlights.length; i++) {
+				if (flashlights[i] >= flashlightsSlowIndex) {
+					flashlights[i]++;
+
+					if (flashlights[i] > flux.length) {
+						flashlights.splice(i, 1); // remove light
+					}
+				}
+			}
+
+			flashlightsSlowFramesCount = 0;
+		}
+
+
 		console.log(flashlights);
 
 		for(var h in flux) {
        if (flux.hasOwnProperty(h)) {
-        stroke(255);
+        // stroke(255, 255, 255, 0);
+				noStroke();
         strokeWeight(5);
-        fill(255);
+        fill(255, 255, 255, 120);
           //console.log(flux[h][1]);
 
 					if (flashlights.indexOf(parseInt(h)) != -1) {
-						stroke(255,0,0);
+						stroke(255, 255, 255);
+						strokeWeight(10);
+						fill(255, 255, 255);
 					}
         ellipse(flux[h][0], flux[h][1], 10, 10);
        }
     }
-		*/
+
     //rectMode(CENTER);
 
 
@@ -576,5 +605,15 @@ window.onload = function() {
     sliderCurrentIndex++;
     if(sliderCurrentIndex == 25) {sliderCurrentIndex = 0}
     document.getElementsByTagName('input')[0].value = sliderCurrentIndex;
-  }, 800);
+  },2500);
+	createLight();
 };
+
+
+function createLight() {
+	flashlights.unshift(-1);
+
+	setTimeout(function(){
+		createLight();
+	}, flashlightsDensity);
+}
