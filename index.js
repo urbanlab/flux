04e242e63b5feacf2a720ed62b0ebea52da1cf68
qdieppe <quentin.dieppe@epitech.eu>
2018-04-03 12:25:58 +0200

    index.js

--- index.js
+++ index.js
@@ -50,32 +50,41 @@ function time2index(time) {
     }
 }
 
-/* La fonction scale_histogram transforme un histogram dont la somme vaut N en un histogram dont la somme vaut TotalPeople,
- * on repartie donc notre population de 5000 personnes selon les proportions definie par prob_array (histogramme des durées) */
+/* La fonction scale_histogram transforme un histogramme dont la somme vaut N
+ * en un histogram dont la somme vaut TotalPeople, on repartie donc notre population de 5000 personnes 
+ * selon les proportions definie par prob_array (histogramme des durées) */
 prob_array = algo.scale_histogram(get_prob_array('./ressources/prob_file.json'), totalPeople);
 probabilite = get_prob_array('./ressources/prob_file.json'); //ouverture de l'histogramme des temps de trajets avec get_prob_array
-console.log("Scaled traffic: ", prob_array);
+console.log("Scaled traffic: ", prob_array); //Affichage de prob_array
 
 //Root listener
 app.use('/assets-visu', express.static('views/visu'));
 app.use('/mobile/libs', express.static('views/mobile/libs'));
 app.use('/mobile/assets', express.static('views/mobile/assets'));
 
+
+/* app.get() contient une fonction callback qui sera exécuté lorsque le client demandera
+ * la PATH spécifié, ici '/' représente la racine du site */
 app.get('/', function(req, res) {
     res.send("Hello World!");
 });
 
 //Mobile view listener
+/* La PATH '/mobile/' contient la page mobile de base mais il est necessaire 
+ * d'y spécifier un profil. exemple : '/mobile/?profile=tassin' 
+ * (Ce sont les pages de l'application en ligne) */
 app.get('/mobile/', function(req, res) {
     res.sendfile('views/mobile/index.html');
 });
 
 //Visu view listener
+/* La PATH '/visu/' represente la page de visualisation */
 app.get('/visu/', function(req, res) {
     res.sendfile('views/visu/index.html');
 });
 
 //Server instantiation
+/* Permet de demander au serveur d'éccouter sur le port 3000 */
 server.listen(3000, function() {
     console.log('FLUX Server started on port 3000');
 });
@@ -84,47 +93,54 @@ server.listen(3000, function() {
 var visu = io.of('/visu');
 
 //Visu socket listener
+/* Cette fonction permet de definir une fonction de callback éxécuté lors d'une connection 
+ * sur la socket visu */
 visu.on('connection', function(socket) {
     console.log('someone connected on visu');
-    algo.updateVisu(visu, profile, sockets);
+	/* La fonction updataVisu est definie dans scripts/algo.js, 
+	 * elle met as jour les éléments important pour la visualisation 
+	 * (envois de l'histogramme calculé et des durées de trajets) */
+    algo.updateVisu(visu, profile, sockets); 
 });
 
 //Declaring mobile socket
 var mobile = io.of('/mobile');
 
 //Mobile socket listener
+/* Cette fonction permet de definir une fonction de callback éxécuté lors d'une connection 
+ * sur la socket mobile */
 mobile.on('connection', function(socket) {
     var clientId;
-    socket.on('client', function (id) {
+    socket.on('client', function (id) { //cette fonction attent une requete de type 'client' qui lui transmet dans id une chaine de caractere contenant "tassin", "lyon7" ou "villeurbanne"
         clientId = id;
-        console.log("sending profile for",id," : ",profile[id]);
-        sockets[clientId] = socket;
-    	socket.emit('profile', profile[id]);
+        console.log("sending profile for",id," : ",profile[id]); //Affiche sur la console l'objet profile correspondant à l'id spécifié
+        sockets[clientId] = socket; //sockets est un tableau de socket, contenant les socket de chaque client (initialisé lors de leur connection)
+    	socket.emit('profile', profile[id]); //envois au site le profile lors d'une connection
     });
 
     //On start message stock the value in client[id]
-    socket.on('start', function(v) {
+    socket.on('start', function(v) { //receptionne l'heure v de type xx:xx qui defini le debut de la plage horaire disponinble d'un client
         if(clientId) {
-            profile[clientId] = profile[clientId] || {};
-            var startTimeIndex = time2index(v);
-            console.log('client[',clientId,'].start=',v," (",startTimeIndex,")");
-            profile[clientId]['start'] = startTimeIndex;
-            algo.updateVisu(visu, profile, sockets);
+            profile[clientId] = profile[clientId] || {}; //c'est pas moi qui est codé cette ligne de singe, privilégié a l'avenir un if (profile[clientId] === undefined) {... (gestion de l'erreur) }
+            var startTimeIndex = time2index(v); //transforme l'heure v en index
+            console.log('client[',clientId,'].start=',v," (",startTimeIndex,")"); 
+            profile[clientId]['start'] = startTimeIndex; //initialise le 'start' d'un profile (avec l'index du debut de sa plage horaire)
+            algo.updateVisu(visu, profile, sockets); //lance updateVisu defini dans algo.js qui met a jour 
         }
     });
 
     //On end message stock the value in client[id]
     socket.on('end', function(v) {
         if(clientId) {
-            profile[clientId] = profile[clientId] || {};
-            var endTimeIndex = time2index(v);
+            profile[clientId] = profile[clientId] || {}; //remplacer cette ligne par une gestion d'erreur propre if (profile[clientId] === undefined) {... (gestion de l'erreur)}
+            var endTimeIndex = time2index(v); //transforme l'heure de fin de plage horaire du client du type 'xx:xx' en index
             console.log('client[',clientId,'].end=',v," (",endTimeIndex,")");
-            profile[clientId]['end'] = endTimeIndex;
-            algo.updateVisu(visu, profile, sockets);
+            profile[clientId]['end'] = endTimeIndex; //initialise le 'end' d'un progile (avec l'index de l'heure de fin de sa plage horaire)
+            algo.updateVisu(visu, profile, sockets); //updateVisu
         }
     });
 
     socket.on('disconnect', function() {
-        delete sockets[clientId];
+        delete sockets[clientId]; //detruit une socket lors d'une deconnection
     });
 });
